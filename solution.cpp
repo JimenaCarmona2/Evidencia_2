c4780682126a333f693f45f3372cb82eb3754f0f
JimenaCarmona2 <jimenagarcia0284@gmail.com>
2024-11-18 20:42:02 -0600

    first commit

---
+++ solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+struct Edge {
+    int u, v, weight;
+};
+
+int main(int argc, char *argv[]) {
+    string filename = argv[1];
+
+    vector<vector<int>> AdjMatrixDistance;
+    vector<vector<int>> AdjMatrixData;
+    int V;
+
+    return 0;
+}
