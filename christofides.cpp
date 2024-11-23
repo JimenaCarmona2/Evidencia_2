56677d8073f5a264ce3fd62b9c26e40ff023801f
Fidel Morales <fidel.morales.briones@gmail.com>
2024-11-23 14:54:57 -0600

    added christofides file

---
+++ christofides.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "kruskal.h"
+
+using namespace std;
+
+void christofidesTSP(vector<Edge>& mstEdges) {
+    vector<vector<int>> mst;
+
+    for (Edge edge : mstEdges) {
+        for (int weight : row) {
+            cout << weight << " ";
+            outfile << weight << " ";
+        }
+        cout << endl;
+        outfile << endl;
+    }
+
+}
+
